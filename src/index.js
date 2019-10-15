8c7ce4344453ca1c0955ac1484724d82a6c04fe8
Nicole Dow <nikki.dow71@gmail.com>
2019-10-15 12:24:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+const button = document.getElementById('create-new-task-button');
+
+button.addEventListener('click', addItemToList);
+
+function addItemToList() {
+  let ul = document.getElementById('tasks');
+  let li = document.createElement('li');
+  li.className = 'task-item'
+  let newButton = document.createElement('button');
+  newButton.className = 'delete-btn';
+  newButton.innerText = 'X';
+
+  ul.appendChild(li);
+  li.innerText = document.getElementById('new-task-description').value;
+  li.appendChild(newButton)
+}
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+document.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
+
+
+
+
+
+
